d8a4aa719da8d5369e40f3ff3243fa2be36c03e3
Rafael-Meza <72485508+Rafael-Meza@users.noreply.github.com>
2022-05-03 20:30:17 -0800

    Avance

--- IA VS COVID/Form1.Designer.cs
+++ IA VS COVID/Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace IA_VS_COVID
+﻿using IA_VS_COVID.Constants;
+
+namespace IA_VS_COVID
 {
     partial class Form1
     {
@@ -28,12 +30,50 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.RBtn_Comenzar = new IA_VS_COVID.Constants.RoundedButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // RBtn_Comenzar
+            // 
+            this.RBtn_Comenzar.FlatAppearance.BorderSize = 0;
+            this.RBtn_Comenzar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.RBtn_Comenzar.Location = new System.Drawing.Point(224, 342);
+            this.RBtn_Comenzar.Margin = new System.Windows.Forms.Padding(0);
+            this.RBtn_Comenzar.Name = "RBtn_Comenzar";
+            this.RBtn_Comenzar.Size = new System.Drawing.Size(142, 58);
+            this.RBtn_Comenzar.TabIndex = 1;
+            this.RBtn_Comenzar.Text = "roundedButton1";
+            this.RBtn_Comenzar.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(1, 338);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(115, 115);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.RBtn_Comenzar);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private RoundedButton RBtn_Comenzar;
+        private PictureBox pictureBox1;
     }
 }
--- IA VS COVID/Form1.cs
+++ IA VS COVID/Form1.cs
@@ -1,3 +1,5 @@
+using IA_VS_COVID.Constants;
+
 namespace IA_VS_COVID
 {
     public partial class Form1 : Form
@@ -5,6 +7,11 @@ namespace IA_VS_COVID
         public Form1()
         {
             InitializeComponent();
+            this.BackgroundImage = Image.FromFile(AppGraphics.HOME);
+            RBtn_Comenzar.BackColor = AppColors.blue;
+            pictureBox1.BackgroundImage = Image.FromFile(AppGraphics.VIRUS);
+            pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
         }
+        
     }
 }
