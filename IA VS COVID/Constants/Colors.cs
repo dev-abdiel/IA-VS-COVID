84cb68632bc9a2159674797a4b573ed4c10acb2a
Jesus Coronado <jesus.coronado@geekbears.com>
2022-05-03 18:59:23 -0700

    inital structure

---
+++ IA VS COVID/Constants/Colors.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IA_VS_COVID.Constants
+{
+    public abstract class AppColors
+    {
+        public static readonly Color blue = ColorTranslator.FromHtml("#2982DF");
+        public static readonly Color black = ColorTranslator.FromHtml("#251D3A");
+        public static readonly Color white = ColorTranslator.FromHtml("#F5F5F5");
+        public static readonly Color red = ColorTranslator.FromHtml("#EB5353");
+        public static readonly Color green = ColorTranslator.FromHtml("#36AE7C");
+    }
+}
+
---
+++ IA VS COVID/Constants/Graphics.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IA_VS_COVID.Constants
+{
+    public abstract class AppGraphics
+    {
+        private readonly static String _prefix = "../../../Assets/";
+
+        public readonly static String APP_ICON = $"{_prefix}/app_icon.png";
+        public readonly static String HOME = $"{_prefix}/1.png";
+        public readonly static String COVID_EXPLANATION = $"{_prefix}/2.png";
+        public readonly static String PERSONAL_DATA = $"{_prefix}/3.png";
+        public readonly static String BACKGROUND = $"{_prefix}/4.png";
+        public readonly static String VIRUS = $"{_prefix}/virus.png";
+    }
+}
