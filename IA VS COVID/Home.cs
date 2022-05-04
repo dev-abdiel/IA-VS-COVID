61ca9887d3cf350390004679528830a1500cefdd
JOSE MARIA JUAREZ MEJIA <l18303028@puertopenasco.tecnm.mx>
2022-05-04 12:51:15 -0700

    AvancePersonalData

--- IA VS COVID/Home.cs
+++ IA VS COVID/Home.cs
@@ -12,9 +12,15 @@ namespace IA_VS_COVID
             RBtn_Comenzar.Text = AppTexts.COMENZAR;
             RBtn_Comenzar.ForeColor = AppColors.white;
             RBtn_Comenzar.Font = new Font(RBtn_Comenzar.Font.Name, 14, FontStyle.Bold);
+            RBtn_Comenzar.Click += this.RBtn_Comenzar_Click;
             Pb_Informacion.BackgroundImage = Image.FromFile(AppGraphics.VIRUS);
             Pb_Informacion.BackgroundImageLayout = ImageLayout.Stretch;
         }
-        
+        private void RBtn_Comenzar_Click(object? sender, EventArgs? e)
+        {
+            PersonalData form = new PersonalData();
+            form.Show();
+
+        }
     }
 }
