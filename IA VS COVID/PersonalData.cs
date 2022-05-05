3f8b3280cd2f80fc806320bb9beb3df9ae0a7a7d
Jesus Coronado <43246999+dev-abdiel@users.noreply.github.com>
2022-05-04 19:16:53 -0700

    AvancePersonalData (#2)
    Co-authored-by: JOSE MARIA JUAREZ MEJIA <l18303028@puertopenasco.tecnm.mx>

---
+++ IA VS COVID/PersonalData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IA_VS_COVID
+{
+    public partial class PersonalData : Form
+    {
+        public PersonalData()
+        {
+            InitializeComponent();
+
+            this.BackgroundImage = Image.FromFile(AppGraphics.PERSONAL_DATA);
+
+            label firstlab = new label();
+            firstlab.Text = ("NOMBRE:  ");
+            firstlab.Location = new Point(222, 90);
+
+            TextBox txtBox = new TextBox();
+
+
+
+
+
+
+
+
+        }
+
+    }
+}
